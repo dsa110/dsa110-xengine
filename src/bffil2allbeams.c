0a09a8a4564844009e2c9e93b043c3412ea772b5
ubuntu <ubuntu@pm.me>
2020-12-10 22:01:02 +0000

    save status

---
+++ src/bffil2allbeams.c
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using std::cout;
+using std::cerr;
+using std::endl;
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <syslog.h>
+#include <pthread.h>
+
+#include <mma.h>
+#include <cuda.h>
+#include "cuda_fp16.h"
+//#include "dada_cuda.h"
+#include "dada_client.h"
+#include "dada_def.h"
+#include "dada_hdu.h"
+#include "multilog.h"
+#include "ipcio.h"
+#include "ipcbuf.h"
+#include "dada_affinity.h"
+#include "ascii_header.h"
+#include "dsaX_def.h"
+#include <thrust/device_ptr.h>
+#include <thrust/fill.h>
+
+#include <cuda_runtime_api.h>
+using namespace nvcuda;
+
+// global variables
+int DEBUG = 0;
