82fd6f71613f880978a6c43e8568d6c399d2173a
ubuntu <ubuntu@pm.me>
2021-08-17 22:58:19 +0000

    bf weights

--- src/dsaX_beamformer.cu
+++ src/dsaX_beamformer.cu
@@ -487,13 +487,13 @@ int init_weights(char * fnam, float *antpos, float *weights, char *flagants) {
   fread(antpos,64*sizeof(float),1,fin);
   fread(weights,64*NW*2*2*sizeof(float),1,fin);
   float wnorm;
-  for (int i=0;i<64*NW*2;i++) {
+  /*for (int i=0;i<64*NW*2;i++) {
     wnorm = sqrt(weights[2*i]*weights[2*i] + weights[2*i+1]*weights[2*i+1]);
     if (wnorm!=0.0) {
       weights[2*i] /= wnorm;
       weights[2*i+1] /= wnorm;
     }
-  }
+    }*/
 	
 
   int ant;
@@ -947,7 +947,7 @@ int main (int argc, char *argv[]) {
 	syslog(LOG_INFO,"coeff %d %g",i,bp[i]);
 	if (bp[i]!=0.) {
 	  bp[i] /= 48.*nints; 
-	  bp[i] = 4.*128./bp[i];
+	  bp[i] = 3.*128./bp[i];
 	}
       }
       cudaMemcpy(d_bp, bp, sizeof(float)*256, cudaMemcpyHostToDevice);
