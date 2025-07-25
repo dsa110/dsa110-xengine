46c4a26bfecd3f79ea638786ed62a0b8f11c4315
VR-DSA <vikram@caltech.edu>
2025-07-25 19:27:23 +0000

    hella

--- src/dsaX_hella.cu
+++ src/dsaX_hella.cu
@@ -1278,15 +1278,13 @@ float bandpass_flag(pinfo * p, half * data, float * fperc) {
 
     syslog(LOG_INFO,"DUMPING FOR FIRST TIME");
     cudaMalloc(&dtmpf, NCHAN * p->NTIME * sizeof(float));
-    dtmpf = (float *)malloc(sizeof(float)*p->NTIME*NCHAN);
+    htmp = (float *)malloc(sizeof(float)*p->NTIME*NCHAN);
     extract_data<<<p->batch_stride*NCHAN/32,32>>>(data,dtmpf,p->NTIME,p->batch_stride);
     cudaDeviceSynchronize();
     cudaMemcpy(htmp,dtmpf,p->NTIME*NCHAN*4,cudaMemcpyDeviceToHost);
-    ftmp = fopen("/home/ubuntu/data/d2.tmp","w");
-    for (int i=0;i<NCHAN * p->NTIME;i++)
-      fprintf(ftmp,"%g\n",htmp[i]);
+    ftmp = fopen("/home/ubuntu/data/d1.tmp","wb");
+    fwrite(htmp,4,p->NTIME*NCHAN,ftmp);
     fclose(ftmp);
-    syslog(LOG_INFO,"%d\n",NCHAN * p->NTIME);
     
   }
   
@@ -1299,9 +1297,8 @@ float bandpass_flag(pinfo * p, half * data, float * fperc) {
     extract_data<<<p->batch_stride*NCHAN/32,32>>>(data,dtmpf,p->NTIME,p->batch_stride);
     cudaDeviceSynchronize();
     cudaMemcpy(htmp,dtmpf,p->NTIME*NCHAN*4,cudaMemcpyDeviceToHost);
-    ftmp = fopen("/home/ubuntu/data/d2.tmp","w");
-    for (int i=0;i<NCHAN * p->NTIME;i++)
-      fprintf(ftmp,"%g\n",htmp[i]);
+    ftmp = fopen("/home/ubuntu/data/d2.tmp","wb");
+    fwrite(htmp,4,p->NTIME*NCHAN,ftmp);
     fclose(ftmp);
 
     doDump = 0;
